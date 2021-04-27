5b99793c11d346e3675160248e68ee8780f93f53
Tyler Stern <tstern@paradromics.com>
2021-04-27 15:11:52 -0500

    Format
    Tidied whitespace and removed debugging statements/unit tests

--- pixelselect.go
+++ pixelselect.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	fmt.Println("pixel select version 0.2")
-	PixelSelect(10, 5)
+	PixelSelect(15, 3)
 }
 
 // PixelSelect accepts the dimensions of the rectangle from which to select pixels &
@@ -22,7 +22,6 @@ func PixelSelect(rc_wid, rc_hei int) {
 	}
 
 	seq_len := rc_hei * rc_wid
-	//fmt.Println("expected number of elements: " + strconv.Itoa(seq_len))
 
 	// Create an array of vertex names
 	v_names := make([]string, seq_len)
@@ -33,9 +32,6 @@ func PixelSelect(rc_wid, rc_hei int) {
 			v_ind += 1
 		}
 	}
-
-	//fmt.Println("v_names length: " + strconv.Itoa(len(v_names)))
-	//fmt.Println("First element: " + v_names[0])
 
 	// Create index
 	sh_ind := make([]int, seq_len)
@@ -53,7 +49,6 @@ func PixelSelect(rc_wid, rc_hei int) {
 	}
 
 	// Print each vertex array element in the order of the shuffled index
-	// NB: currently prints sh_ind elements in order
 	for pr_ind := 0; pr_ind < seq_len; pr_ind++ {
 		fmt.Println(v_names[sh_ind[pr_ind]])
 	}
