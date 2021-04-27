408fd868e200eaed34da3c0eb4e139fdef692ae5
Tyler Stern <tstern@paradromics.com>
2021-04-27 14:37:08 -0500

    Create index
    Create index and print elements for debugging

--- pixelselect.go
+++ pixelselect.go
@@ -19,10 +19,11 @@ func PixelSelect(rc_wid, rc_hei int) {
 		panic(errors.New("Rectangle dimensions must be positive numbers."))
 	}
 
-	//fmt.Println("expected number of elements: " + strconv.Itoa(rc_hei*rc_wid))
+	seq_len := rc_hei * rc_wid
+	//fmt.Println("expected number of elements: " + strconv.Itoa(seq_len))
 
 	// Create an array of vertex names
-	v_names := make([]string, rc_hei*rc_wid)
+	v_names := make([]string, seq_len)
 	v_ind := 0
 	for W := 0; W < rc_wid; W++ {
 		for H := 0; H < rc_hei; H++ {
@@ -35,13 +36,17 @@ func PixelSelect(rc_wid, rc_hei int) {
 	//fmt.Println("First element: " + v_names[0])
 
 	// Create index
+	sh_ind := make([]int, seq_len)
+	for a := 0; a < seq_len; a++ {
+		sh_ind[a] = a
+	}
 
 	// Shuffle index
 
 	// Print each vertex array element in the order of the shuffled index
-	// NB: currently prints in original order
-	for pr_ind := 0; pr_ind < v_ind; pr_ind++ {
-		fmt.Println(v_names[pr_ind])
+	// NB: currently prints sh_ind elements
+	for pr_ind := 0; pr_ind < seq_len; pr_ind++ {
+		fmt.Println(sh_ind[pr_ind])
 	}
 
 	return
